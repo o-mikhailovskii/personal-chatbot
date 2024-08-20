67f936f89355f756f79c3ef5a15d4b35bb06506f
Oleg Mikhailovskii <o.mikhaylovskiy+github+misc@gmail.com>
2024-08-20 10:30:15 +0200

    WIP: invoke Gradio or Tkinter depending on the input argument

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-from tkinter import Tk
-
-from personal_chatbot.chatbot_gui import Chatbot
+import sys
 
 
-def start_main_window():
+def start_tk_interface():
+    """Start the Tkinter-based chatbot interface."""
+    from tkinter import Tk
+
+    from personal_chatbot.chatbot_gui import Chatbot
+
     root = Tk()
     chatbot = Chatbot(root)
     root.protocol("WM_DELETE_WINDOW", chatbot.on_exit)
     root.mainloop()
 
 
+def start_gradio_interface():
+    """Start the Gradio-based chatbot interface."""
+    from personal_chatbot.chatbot_gr import GradioChatbot
+
+    chatbot = GradioChatbot()
+    chatbot.launch()
+
+
 if __name__ == "__main__":
-    start_main_window()
+    if len(sys.argv) > 1 and sys.argv[1] == "gradio":
+        start_gradio_interface()
+    else:
+        start_tk_interface()
